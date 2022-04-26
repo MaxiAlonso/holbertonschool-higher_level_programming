04dd86ed7288d8eca232781457ef5ede3ee08a23
MaxiHBTN <4387@holbertonschool.com>
2022-04-26 18:00:12 +0000

    Task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    count = 0
+    str2 = ""
+    for char in str:
+        if count != n:
+            str2 = str2 + char
+        count += 1
+    print(str2)
