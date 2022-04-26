27f2d7e0bca13e7c81619fe282cc94fa7776e100
MaxiHBTN <4387@holbertonschool.com>
2022-04-26 19:59:16 +0000

    Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -42,5 +42,8 @@ listint_t *insert_node(listint_t **head, int number)
 			count++;
 		}
 	}
-	return (NULL);
+	new_node->n = number;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
