36607463b70dfbbb3cdd93d71f4e37c4b0ee08e9
MaxiHBTN <4387@holbertonschool.com>
2022-04-26 18:34:33 +0000

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     if n >= 0:
         start = str[:n]
         end = str[n+1:]
-        return(start+end)
-    return(str);
+        return(start + end)
+    return(str)
