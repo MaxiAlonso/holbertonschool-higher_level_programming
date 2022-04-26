f6d9d12ae5d0f83d71df69611665be7c11a1b123
MaxiHBTN <4387@holbertonschool.com>
2022-04-26 15:25:43 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for fd in range(0, 9):
     for ld in range(fd, 10):
         if fd != ld:
             if fd != 8:
-                print(f"{fd}{ld}", end=", ")
+                print("{}{}".format(fd, ld), end=", ")
             else:
-                print(f"{fd}{ld}")
+                print("{}{}".format(fd, ld))
