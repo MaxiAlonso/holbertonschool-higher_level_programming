c77ffd724daf35d18d0b5fee77cdd64c86163b7f
MaxiHBTN <4387@holbertonschool.com>
2022-05-02 19:51:55 +0000

    Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list:
-        if idx < 0 or idx >= len(my_list):
-            return my_list
-        else:
-            for i in range(len(my_list)):
-                if i == idx:
-                    del my_list[i]
-            return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     else:
-        return None
+        del my_list[idx]
+        return my_list
