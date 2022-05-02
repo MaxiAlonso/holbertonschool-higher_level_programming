e4ffc9b058b41771c3f4d9536d3b5ef074a63ca9
MaxiHBTN <4387@holbertonschool.com>
2022-05-02 19:01:52 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        tup = (0, None)
+    if len(sentence) == 0:
+        tup = (len(sentence), None)
     else:
         tup = (len(sentence), sentence[0])
     return tup
