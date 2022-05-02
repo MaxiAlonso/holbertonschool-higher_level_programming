7f6a8b158092d8234776a5e2440e91ee9beeeb34
MaxiHBTN <4387@holbertonschool.com>
2022-05-02 15:13:10 +0000

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
-            for element in reversed(my_list):
-                print(f"{element:d}")
+        for element in reversed(my_list):
+            print(f"{element:d}")
