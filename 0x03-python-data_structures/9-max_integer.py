b161a6c444d4aa21d2f29126a2570341b5e34b39
MaxiHBTN <4387@holbertonschool.com>
2022-05-02 19:11:56 +0000

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is not None:
+    if my_list is None:
+        return None
+    else:
         maxnum = my_list[0]
         for num in my_list:
             if num > maxnum:
                 maxnum = num
-    else:
-        maxnum = None
-    return(maxnum)
+        return(maxnum)
