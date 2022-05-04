5cfbdffdc142471d771eac78107ab3665a734460
MaxiHBTN <4387@holbertonschool.com>
2022-05-04 15:12:55 +0000

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy() 
     for ridx, row in enumerate(matrix):
         for cidx, num in enumerate(row):
-            matrix[ridx][cidx] = num ** 2
-    return matrix
+            new_matrix[ridx][cidx] = num ** 2
+    return new_matrix
