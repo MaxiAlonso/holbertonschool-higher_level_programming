0c1ac151189eea3d5210bdde4348f2ef801d6ac0
MaxiHBTN <4387@holbertonschool.com>
2022-05-17 17:32:31 +0000

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Square Class
+    '''
+
+    def __init__(self, size=0):
+        '''
+        Initializes a Square
+
+        Size is private attribute because the size of a square is crucial
+        for a square, many things depend of it (area computation, etc.) and
+        keep it privately is one of the ways to have the control of size.
+        '''
+
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size**2
