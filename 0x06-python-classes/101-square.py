e49fa3f26ff1ff3e78a789f751fddaac21141510
MaxiHBTN <4387@holbertonschool.com>
2022-05-17 22:57:24 +0000

    Task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+'''
+Class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Square Class
+    '''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''
+        Initializes a Square
+
+        Size and position are private attributes
+        because things depend of them and
+        keep them privately is one of the ways to have the control of them.
+        '''
+
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        '''
+        To get the size
+        '''
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        To set the size
+        '''
+
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        '''
+        To get the position
+        '''
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''
+        To set the position
+        '''
+
+        if ((type(value) is not tuple) or (len(value) != 2) or
+                (type(value[0]) is not int) or
+                (type(value[1]) is not int) or
+                (value[0] < 0) or
+                (value[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        '''
+        Calculates and return the area of the square
+        '''
+
+        return self.__size**2
+
+    def my_print(self):
+        '''
+        Prints in stdout the square with the character #
+        '''
+
+        if self.__size == 0:
+            print()
+        else:
+            for row_spaces in range(self.__position[1]):
+                print()
+            for width in range(self.__size):
+                for col_spaces in range(self.__position[0]):
+                    print(" ", end="")
+                for heigth in range(self.__size):
+                    print("#", end="")
+                print()
+
+    def __str__(self):
+        '''
+        Prints in stdout the square with the character #
+        '''
+
+        square = ""
+
+        if self.__size != 0:
+            for row_spaces in range(self.__position[1]):
+                square += "\n"
+            for width in range(self.__size):
+                for col_spaces in range(self.__position[0]):
+                    square += " "
+                for heigth in range(self.__size):
+                    square += "#"
+                if heigth < self.__size:
+                    square += "\n"
+        return square[:-1]
