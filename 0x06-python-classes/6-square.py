ea931159badbfa93c0959b3f2ab35d6b220736bd
MaxiHBTN <4387@holbertonschool.com>
2022-05-17 20:53:44 +0000

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,8 +18,8 @@ class Square:
         keep them privately is one of the ways to have the control of them.
         '''
 
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
