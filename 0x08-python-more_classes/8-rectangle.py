462249769aba436287f8f056802d1a1171582898
MaxiHBTN <4387@holbertonschool.com>
2022-05-18 18:58:26 +0000

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''
-        Initializes a rectangle
+        Initializes a rectangle 
         '''
 
         self.width = width
