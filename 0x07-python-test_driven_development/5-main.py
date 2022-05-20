f5a10746ac7591d4d84938053bfc15086881a0bb
MaxiHBTN <4387@holbertonschool.com>
2022-05-20 16:42:38 +0000

    Task 5 test

--- 0x07-python-test_driven_development/5-main.py
+++ 0x07-python-test_driven_development/5-main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-max_integer = __import__('6-max_integer').max_integer
+text_indentation = __import__('5-text_indentation').text_indentation
 
-print(max_integer([1, 2, 3, 4]))
-print(max_integer([1, 3, 4, 2]))
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
+
