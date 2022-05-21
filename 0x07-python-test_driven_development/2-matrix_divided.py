11dc020f25846293a8a68cbf3b03ba6bc271f043
MaxiHBTN <4387@holbertonschool.com>
2022-05-21 14:52:32 +0000

    Task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,25 +8,28 @@ def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix
     """
-    
+
     new_list = []
+    message = "matrix must be a matrix (list of lists) of integers/floats"
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    else:
-        for row in matrix:
-            if row is not None:
-                lenrow = len(row)
-            else:
-                lenrow = None
-            temp_list = [] 
-            for number in row:
-                if type(number) is not int and type(number) is not float:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                if len(matrix[0]) != lenrow:
-                    raise TypeError("Each row of the matrix must have the same size")
-                temp_list.append((round((number / div), 2)))
-            new_list.append(temp_list)
-        return new_list
+
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError(message)
+
+    for row in matrix:
+        if type(row) is not list or len(row) == 0:
+            raise TypeError(message)
+        if len(matrix[0]) != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        temp_list = []
+        for number in row:
+            if type(number) is not int and type(number) is not float:
+                raise TypeError(message)
+            temp_list.append((round((number / div), 2)))
+        new_list.append(temp_list)
+    return new_list
