122c3b6f3edecb1f127f48f32c8e619d82238776
MaxiHBTN <4387@holbertonschool.com>
2022-05-21 17:10:03 +0000

    Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Function that prints a text with 2 new lines after
+each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+
+    charlist = [".", "?", ":"]
+    spaces = "\n\n"
+    skipspace = 0
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for char in text:
+        if skipspace == 1:
+            skipspace = 0
+            continue
+        print(char, end="")
+        if char in charlist:
+
+            print(spaces, end="")
+            skipspace = 1
