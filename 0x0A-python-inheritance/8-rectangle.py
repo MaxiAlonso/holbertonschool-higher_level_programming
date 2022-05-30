5a52ccc2d491191fa11198e71561d617afe8b2c4
MaxiHBTN <4387@holbertonschool.com>
2022-05-30 21:28:56 +0000

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle(BaseGeometry):
         Instantiation with width and height
         """
 
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
