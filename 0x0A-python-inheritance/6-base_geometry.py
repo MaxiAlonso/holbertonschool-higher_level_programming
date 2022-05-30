d5ed8f896eefbafb0f40104b5cf8b98cb532e523
MaxiHBTN <4387@holbertonschool.com>
2022-05-30 18:19:45 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
