51f0f6f0cfdd8ee81ff81f05fe1f1c9155931d78
MaxiHBTN <4387@holbertonschool.com>
2022-05-30 15:56:50 +0000

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Returns the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
