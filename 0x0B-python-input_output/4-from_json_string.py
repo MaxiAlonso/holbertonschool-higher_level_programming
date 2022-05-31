d44cd85453564da76fa69a5cea5b50afa5ecb416
MaxiHBTN <4387@holbertonschool.com>
2022-05-31 19:46:01 +0000

    Task 5

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """
-Function that returns an object (Python data structure) represented by a JSON string
+Function that returns an object (Python data structure)
+represented by a JSON string
 """
 
 import json
 
+
 def from_json_string(my_str):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string
+    """
+
     return json.loads(my_str)
