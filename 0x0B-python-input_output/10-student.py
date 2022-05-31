408ca940bda1d25b77cb1c8611054c7964c7c18c
MaxiHBTN <4387@holbertonschool.com>
2022-05-31 18:46:32 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    Class Student that defines a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            dic = {}
+            for at in attrs:
+                for key, value in self.__dict__.items():
+                    if key == at:
+                        dic[key] = value
+            return dic
+        else:
+            return (self.__dict__)
