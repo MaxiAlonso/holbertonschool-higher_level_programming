789f4445484d4344f0e38a32d33a042a05cab93a
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 22:15:50 +0000

    Task 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Script that removes all records with a score <= 5 in the table second_table of the database
+-- Command to delete a row from second_table
+DELETE FROM second_table
+WHERE score <= 5;
