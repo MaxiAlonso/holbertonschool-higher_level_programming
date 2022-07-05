6e7d49c1bc4a6c64555a552a5a47a1907b860d29
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 23:27:51 +0000

    Task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,5 +3,6 @@
 --  Database hbtn_0c_0
 --  Table first_table
 --  Field name in first_table
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
