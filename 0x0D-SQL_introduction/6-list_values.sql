07092aa77355f0cb7db70d2ca67fb212a4779bec
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 17:53:33 +0000

    Task 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Command that lists all rows of the table first_table from the database hbtn_0c_0 (passed by argument)
+SELECT * FROM first_table
