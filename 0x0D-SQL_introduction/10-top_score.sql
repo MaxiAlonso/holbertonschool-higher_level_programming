fbc76ab95e5b4f30ac02d67a38e9fa97e5ff75b5
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 18:46:11 +0000

    Task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Command to list all records of the second table of the databes hbtn_0c_0 (passe by argument)
+SELECT score, name
+    FROM second_table
+    ORDER BY score DESC;
