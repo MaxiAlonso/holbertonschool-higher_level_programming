729e93f358792a7aa7ba651d9636144e79e96250
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 17:45:07 +0000

    Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Command to show the full description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE first_table
