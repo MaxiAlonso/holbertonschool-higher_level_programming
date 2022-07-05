d791b13008ff7e3fbc66a753635b8b88a5c24967
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 21:42:13 +0000

    Task 12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Script that updates the score of Bob to 10 in the table second_table.
+-- Command to update the score of Bob in the second table 
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
