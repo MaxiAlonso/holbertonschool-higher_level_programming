ce16bd04a4f35fc3fae0411fb77707f10977ce0c
MaxiHBTN <4387@holbertonschool.com>
2022-07-05 22:30:41 +0000

    Task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Command that retrieves the number of entries with the same score.
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
