395e25ce43a3e572b636c44004b04cdb30e33398
MaxiHBTN <4387@holbertonschool.com>
2022-07-06 17:40:55 +0000

    Task 4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table id_not_null on your MySQL server.
+-- Query to create table:
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+    );
