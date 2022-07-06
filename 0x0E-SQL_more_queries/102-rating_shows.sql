146069c512796c857800b55aa8695bed5c2faf04
MaxiHBTN <4387@holbertonschool.com>
2022-07-06 23:06:47 +0000

    Task 19

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- Script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Query:
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating' FROM tv_shows
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
