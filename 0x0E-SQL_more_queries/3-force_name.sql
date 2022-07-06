dc0c25603eee466c4ee6ca1dcc3c264c30fe1814
MaxiHBTN <4387@holbertonschool.com>
2022-07-06 17:20:17 +0000

    Task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Script that creates the table force_name on your MySQL server.
+-- Query to create the table:
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
+    name VARCHAR(256) NOT NULL
+    );
+      
