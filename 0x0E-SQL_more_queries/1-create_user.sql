b828db93eca956984aefccc0471c4c666498e80d
MaxiHBTN <4387@holbertonschool.com>
2022-07-06 16:35:22 +0000

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1.
--- Query:
-GRANT ALL PRIVILEGES ON *.* to user_0d_1@localhost;
+-- Query to create user:
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+-- Query to grant privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
