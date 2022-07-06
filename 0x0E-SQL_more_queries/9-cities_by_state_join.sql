77e97aa7c93618eb8e1830781ca7c7111d16795a
MaxiHBTN <4387@holbertonschool.com>
2022-07-06 19:45:52 +0000

    Task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa.
+-- Query:
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
