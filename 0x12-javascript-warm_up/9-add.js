6f7276d339a253d6ca3060349ee7a534016353a6
MaxiHBTN <4387@holbertonschool.com>
2022-08-01 21:31:51 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
 const args = process.argv;
 if (isNaN(Number(args[2]))) {
   console.log(NaN);
 } else if (isNaN(Number(args[3]))) {
   console.log(NaN);
 } else {
-  console.log(Number(args[2]) + Number(args[3]));
+  console.log(add(Number(args[2]), Number(args[3])));
 }
