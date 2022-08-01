fc4ff2635ecb213a8bfd9d9cb1e8ab9b3166a86c
MaxiHBTN <4387@holbertonschool.com>
2022-08-01 18:23:52 +0000

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const args = process.argv;
+const n = Number(args[2]);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  const fact = factorial(n);
+  console.log(fact);
+}
