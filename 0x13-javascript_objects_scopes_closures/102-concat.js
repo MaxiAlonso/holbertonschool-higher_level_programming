8b0c2040cadc6be9e531019d1273f880668f69bd
MaxiHBTN <4387@holbertonschool.com>
2022-08-02 18:47:57 +0000

    Task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+const textA = fs.readFileSync(args[2]);
+const textB = fs.readFileSync(args[3]);
+fs.writeFileSync(args[4], textA + '\n' + textB + '\n');
