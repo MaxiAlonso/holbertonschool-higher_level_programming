86d2636713750f85d4a3a4a8ce3f36b0a87c1c6d
MaxiHBTN <4387@holbertonschool.com>
2022-08-05 16:32:57 +0000

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Script that takes in arguments and displays all
+values in the states table of hbtn_0e_0_usa
+where name matches the argument. 
+Safe from MySQL injections!
+"""
 
 import MySQLdb
 from sys import argv
