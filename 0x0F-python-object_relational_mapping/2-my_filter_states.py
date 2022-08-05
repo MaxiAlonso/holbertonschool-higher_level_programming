3bff762828c5ea8bbbffacae9a1c7385af06be61
MaxiHBTN <4387@holbertonschool.com>
2022-08-05 16:32:51 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 import MySQLdb
 from sys import argv
