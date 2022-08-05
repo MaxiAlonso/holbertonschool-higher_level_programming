866973738cdd79fbb3fe852d1c665becb27a9584
MaxiHBTN <4387@holbertonschool.com>
2022-08-05 16:24:04 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,20 +3,24 @@
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
-state = argv[4]
-db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        port=3306,
-        passwd=password,
-        db=database)
+if __name__ == "__main__": 
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state = argv[4]
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            port=3306,
+            passwd=password,
+            db=database)
 
-cur = db.cursor()
-cur.execute(f"SELECT * FROM states WHERE name LIKE '{state}'\
-     ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name LIKE '{state}'\
+            ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
