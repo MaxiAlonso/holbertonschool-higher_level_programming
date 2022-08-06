f6821a0041b491d471cfcfe27a4b2b3acc26300e
MaxiHBTN <4387@holbertonschool.com>
2022-08-06 14:37:02 +0000

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,23 +7,23 @@ with N (upper N) from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":    
-        username = argv[1]
-        password = argv[2]
-        database = argv[3]
-        db = MySQLdb.connect(
-                host="localhost",
-                user=username,
-                port=3306,
-                passwd=password,
-                db=database)
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            port=3306,
+            passwd=password,
+            db=database)
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
-                ORDER BY states.id ASC")
-        rows = cur.fetchall()
-        for row in rows:
-                print(row)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+            ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
