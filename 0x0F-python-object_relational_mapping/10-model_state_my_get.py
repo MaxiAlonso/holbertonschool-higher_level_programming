e789a689e143d53129365ece15086954db18717b
MaxiHBTN <4387@holbertonschool.com>
2022-08-06 18:16:10 +0000

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa
+"""
+
+from os import stat
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_search = argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name.like(state_search)).all()
+    for state in states:
+        if state is None:
+            print("Not found")
+        else:
+            print(state.id)
+            
