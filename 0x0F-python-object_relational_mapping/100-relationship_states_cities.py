ce2c3944bb829fe6547a87eeb16eb1a0c5e75c42
MaxiHBTN <4387@holbertonschool.com>
2022-08-07 16:44:01 +0000

    Task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco", state=new_state)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
