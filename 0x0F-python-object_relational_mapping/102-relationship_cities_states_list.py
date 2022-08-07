5977b0ad1fb2d703ac8806d4f4a1f90f4202ce19
MaxiHBTN <4387@holbertonschool.com>
2022-08-07 17:11:17 +0000

    Task 17

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all City objects from the database hbtn_0e_101_usa
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
