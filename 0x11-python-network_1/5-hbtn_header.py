cf37f8e5b94ec5d722df9921cbaff26ea524fecd
MaxiHBTN <4387@holbertonschool.com>
2022-08-19 17:24:04 +0000

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.get(url)
+    print(response.headers["X-Request-Id"])
