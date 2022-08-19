7681acc0a2f6f3d3b9a17484f5a752996a4672d8
MaxiHBTN <4387@holbertonschool.com>
2022-08-19 16:22:37 +0000

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         with request.urlopen(req) as response:
             body = response.read()
             print(body.decode('utf-8'))
-    except request.HTTPrror as error:
+    except request.HTTPError as error:
         print("Error code: {}".format(error.code))
