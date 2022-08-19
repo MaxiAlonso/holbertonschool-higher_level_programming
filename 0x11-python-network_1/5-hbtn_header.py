9dd69cecfafe4fe9837ab14df9ddc0dab9d0ebba
MaxiHBTN <4387@holbertonschool.com>
2022-08-19 17:27:07 +0000

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,4 +11,4 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     response = requests.get(url)
-    print(response.headers["X-Request-Id"])
+    print(response.headers.get("X-Request-Id"))
