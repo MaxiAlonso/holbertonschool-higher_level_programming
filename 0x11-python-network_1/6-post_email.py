09ea24322e8a1026b6666bba335c58c07150be63
MaxiHBTN <4387@holbertonschool.com>
2022-08-19 17:36:20 +0000

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter,
+and finally displays the body of the response
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[2]}
+    response = requests.post(url, data=value).text
+    print(response)
