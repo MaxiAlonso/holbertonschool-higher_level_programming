d9b9b503286bc3fd22b570343fd4ea442234184a
MaxiHBTN <4387@holbertonschool.com>
2022-09-12 18:20:27 +0000

    tASK 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-// Script that prints the number of movies where the character “Wedge Antilles” is present.
-
-const argv = process.argv;
+// script that prints the number of movies where the character “Wedge Antilles” is present
+const arg = process.argv;
 const axios = require('axios');
 
-axios.get(argv[2])
-  .then(resp => {
+axios
+  .get(arg[2])
+  .then(res => {
     let count = 0;
-    for (let idx = 0; resp.data.results[idx]; idx++) {
-      if (resp.data.results[idx].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        count++;
+    for (const data of res.data.results) {
+      for (const id of data.characters) {
+        if (id.endsWith('18/')) {
+          count++;
+        }
       }
     }
     console.log(count);
