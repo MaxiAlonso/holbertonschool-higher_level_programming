33eb41549774644b752bb81533778cd8a3d2c193
MaxiHBTN <4387@holbertonschool.com>
2022-09-12 17:46:50 +0000

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,16 +7,10 @@ axios.get(args[2])
   .then(function (response) {
     const films = response.data.results;
     let count = 0;
-    for (const idx in films) {
-      const characters = films[idx].characters;
-      for (const index in characters) {
-        if (characters[index] === 'https://swapi-api.hbtn.io/api/people/18/') {
-          count++;
-        }
+    for (let idx = 0; idx < films.length; idx++) {
+      if (films[idx].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
       }
     }
     console.log(count);
-  })
-  .catch(function (error) {
-    console.log(error);
   });
