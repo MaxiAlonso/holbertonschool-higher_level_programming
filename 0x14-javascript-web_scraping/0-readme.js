95d5d51716bee4d1d990e3551ef29af1291f6839
MaxiHBTN <4387@holbertonschool.com>
2022-09-12 15:05:57 +0000

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs').promises;
+
+const args = process.argv;
+async function readFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath);
+    console.log(data.toString());
+  } catch (error) {
+    console.dir(error)
+  }
+}
+
+readFile(args[2]);
