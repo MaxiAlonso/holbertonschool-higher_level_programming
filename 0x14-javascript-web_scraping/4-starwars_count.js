de40dc47f9168e0c59034c427e114f9c7ec54477
MaxiHBTN <4387@holbertonschool.com>
2022-09-12 16:58:47 +0000

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Script that prints the number of movies where the character “Wedge Antilles” is present.
+
+const axios = require('axios');
+const args = process.argv;
+axios.get(args[2])
+  .then(function (response) {
+    const films = response.data.results;
+    let count = 0;
+    for (const idx in films) {
+      const characters = films[idx].characters;
+      for (const index in characters) {
+        if (characters[index] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  });
